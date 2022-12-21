1943a29ae59c569799f5e6b2cb95dd3203d29c38
Алексей Избышев <aizbyshev@mail.ru>
2022-12-22 00:55:22 +0700

    Добавлен тест параметров

--- src/ShaftGearPlugin/ShaftGearPlugin.UnitTests/ShaftGearParameterTest.cs
+++ src/ShaftGearPlugin/ShaftGearPlugin.UnitTests/ShaftGearParameterTest.cs
@@ -6,7 +6,18 @@ namespace ShaftGearPlugin.UnitTests
 {
     class ShaftGearParameterTest
     {
-        [Test(Description = "Negative Getter Test. Value Less Then MinValue")]
+        [Test(Description = "Positive Getter Test.")]
+        public void TestValueGet_CorrectValue()
+        {
+            var expected = 10;
+
+            var shaftGearParameter = new ShaftGearParameter(10, 5, 20);
+            var actual = shaftGearParameter.Value;
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test(Description = "Negative Getter Test. Value Less Then MinValue.")]
         public void TestValueMinValueGet_IncorrectValue()
         {
             const double minValue = 30;
@@ -19,7 +30,7 @@ namespace ShaftGearPlugin.UnitTests
             Assert.That(actual?.ParamName, Is.EqualTo(expected));
         }
 
-        [Test(Description = "Negative Getter Test. Value Bigger Then MaxValue")]
+        [Test(Description = "Negative Getter Test. Value Bigger Then MaxValue.")]
         public void TestValueMaxValueGet_IncorrectValue()
         {
             const double minValue = 30;
