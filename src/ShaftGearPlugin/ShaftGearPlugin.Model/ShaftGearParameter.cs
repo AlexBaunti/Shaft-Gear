219c01a6a27fabd33c41349d935802c0dc559628
Алексей Избышев <aizbyshev@mail.ru>
2022-12-22 00:50:45 +0700

    Добавлены юнит-тесты для параметров

--- src/ShaftGearPlugin/ShaftGearPlugin.Model/ShaftGearParameter.cs
+++ src/ShaftGearPlugin/ShaftGearPlugin.Model/ShaftGearParameter.cs
@@ -2,7 +2,7 @@
 
 namespace ShaftGearPlugin.Model
 {
-    class ShaftGearParameter
+    public class ShaftGearParameter
     {
         /// <summary>
         /// Значение параметра.
@@ -29,7 +29,7 @@ namespace ShaftGearPlugin.Model
             {
                 if (IsRangeOut(value))
                 {
-                    throw new ArgumentException($"Value Can Be Only Between {_minValue} And {_maxValue}");
+                    throw new ArgumentOutOfRangeException($"Value Can Be Only Between {_minValue} And {_maxValue}");
                 }
                 _value = value;
             }
---
+++ src/ShaftGearPlugin/ShaftGearPlugin.UnitTests/ShaftGearParameterTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using NUnit.Framework;
+using ShaftGearPlugin.Model;
+
+namespace ShaftGearPlugin.UnitTests
+{
+    class ShaftGearParameterTest
+    {
+        [Test(Description = "Negative Getter Test. Value Less Then MinValue")]
+        public void TestValueMinValueGet_IncorrectValue()
+        {
+            const double minValue = 30;
+            const double maxValue = 50;
+            const double value = 25;
+
+            var actual = Assert.Throws< ArgumentOutOfRangeException >(() => new ShaftGearParameter(value, minValue, maxValue));
+            var expected = $"Value Can Be Only Between {minValue} And {maxValue}";
+
+            Assert.That(actual?.ParamName, Is.EqualTo(expected));
+        }
+
+        [Test(Description = "Negative Getter Test. Value Bigger Then MaxValue")]
+        public void TestValueMaxValueGet_IncorrectValue()
+        {
+            const double minValue = 30;
+            const double maxValue = 50;
+            const double value = 60;
+
+            var actual = Assert.Throws<ArgumentOutOfRangeException>(() => new ShaftGearParameter(value, minValue, maxValue));
+            var expected = $"Value Can Be Only Between {minValue} And {maxValue}";
+
+            Assert.That(actual?.GetType(), Is.EqualTo(typeof(ArgumentOutOfRangeException)));
+        }
+    }
+}
